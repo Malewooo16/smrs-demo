c05853ecb370e6be80fc3ed137499c43629f64cf
Malewo16 <elijahmalewo16@gmail.com>
2024-05-30 09:46:53 +0000

    fixed routing

--- app/main-components/HomePage.tsx
+++ app/main-components/HomePage.tsx
@@ -1,6 +1,7 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import Image from 'next/image'
+"use client";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
 // Define the FadeInComponent functional component
 const HomePage = () => {
   // Declare a state variable to track if the component is visible
@@ -14,16 +15,37 @@ const HomePage = () => {
   // Render the component with conditional Tailwind CSS classes for the fade-in effect
   return (
     <div
-      className={` w-screen h-screen flex flex-col items-center justify-center  transition-opacity duration-1000 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0'}`}
+      className={` w-screen -mt-10 h-screen flex flex-col items-center justify-center  transition-opacity duration-1000 ease-in-out ${
+        isVisible ? "opacity-100" : "opacity-0"
+      }`}
     >
-     <div className="flex flex-col justify-center items-center">
-     <Image className='w-30 rounded-full' src='/wma-logo.png' alt={"logo"} height={150} width={150} priority = {true} />
-     <h1 className="text-3xl"> Welcome To SMRS </h1>
-    </div>
-    <div className="flex flex-col  lg:flex-row  w-full max-w-xl items-center justify-between mt-4">
+      <div className="flex flex-col justify-center items-center">
+        <Image
+          className="w-30 rounded-full"
+          src="/wma-logo.png"
+          alt={"logo"}
+          height={150}
+          width={150}
+          priority={true}
+        />
+        <h1 className="text-3xl"> Welcome To SMRS </h1>
+      </div>
+      <div className="flex flex-col  lg:flex-row  max-w-xl items-center justify-between mt-4">
         {" "}
-        <button className="btn btn-info min-w-40 my-2"> SignUp </button>{" "}
-        <button className="btn btn-info min-w-40 my-2"> Login </button>{" "}
+        <Link href={`/register`}>
+          {" "}
+          <button className="btn btn-info min-w-40 my-2 mx-2">
+            {" "}
+            SignUp{" "}
+          </button>{" "}
+        </Link>
+        <Link href={`/login`}>
+          {" "}
+          <button className="btn btn-info min-w-40 my-2 mx-2">
+            {" "}
+            Login{" "}
+          </button>{" "}
+        </Link>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-
-
-import ThemeProvider from "./main-components/ThemeProvider"
-import LoginForm from "./main-components/LoginForm"
-import Transition from "./main-components/TransitionTest"
-import HomePage from "./main-components/HomePage"
-
+import ThemeProvider from "./main-components/ThemeProvider";
+import LoginForm from "./main-components/LoginForm";
+import Transition from "./main-components/TransitionTest";
+import HomePage from "./main-components/HomePage";
 
 export default async function Home() {
   //const testPrisma = await fetchTestWorkflowsPerUser("malewoodev")
   return (
-    <main className="flex flex-col p-4  " >
+    <main className="flex flex-col p-4">
       <div className="flex justify-end w-full">
-        
-      <ThemeProvider/>
+        <ThemeProvider />
       </div>
-      
+
       <HomePage />
     </main>
-  )
+  );
 }
